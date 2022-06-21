1e6a3d13bed438f516cebb8dc2f95dee674c63ce
Max Winterstein <MaxWinterstein@users.noreply.github.com>
2022-06-21 11:55:57 +0200

    Read requirements from .in file

--- setup.py
+++ setup.py
@@ -7,17 +7,5 @@ setup(
     author="Max Winterstein",
     author_email="github@winterstein.mx",
     packages=["toogoodtogo_ha_mqtt_bridge"],  # would be the same as name
-    install_requires=[
-        "paho-mqtt",
-        "dynaconf",
-        "tgtg",
-        "coloredlogs",
-        "tenacity",
-        "arrow",
-        "croniter",
-        "google-play-scraper",
-        "random_user_agent",
-        "packaging",
-        "freezegun",
-    ],
+    # install_requires=[], # disabled for now, see requirements.in
 )
