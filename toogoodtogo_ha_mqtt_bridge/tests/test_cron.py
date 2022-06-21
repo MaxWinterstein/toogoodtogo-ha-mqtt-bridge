4f0eb33c86c5bc5362f1826fdb3ed0cd897b24c2
Max Winterstein <MaxWinterstein@users.noreply.github.com>
2022-06-21 10:18:16 +0200

    Add failing test for demonstration purpose

--- toogoodtogo_ha_mqtt_bridge/tests/test_cron.py
+++ toogoodtogo_ha_mqtt_bridge/tests/test_cron.py
@@ -1,2 +1,19 @@
-def test_dummy():
-    pass
+from freezegun import freeze_time
+
+from toogoodtogo_ha_mqtt_bridge.config import settings
+from toogoodtogo_ha_mqtt_bridge.main import calc_next_run
+
+
+# freeze time to specific moment for stable test, tz_offset might not reflect DST
+@freeze_time("2022-01-01 17:00:00", tz_offset=1)
+def test_calc_next_run():
+
+    # When we set the  polling interval to every 10 minutes
+    settings["tgtg"] = {"polling_schedule": "*/10 * * * *"}
+
+    # and calculate the seconds to sleep until next fetch
+    sleep_seconds = calc_next_run()
+
+    # we would expect to sleep less than 10 minutes
+    print(f"Would sleep {sleep_seconds / 60:.3} minutes")
+    assert sleep_seconds <= 60 * 10
