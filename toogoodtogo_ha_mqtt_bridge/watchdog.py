a8e7ec8bec88931234debd3b4597c627fac9562f
Max Winterstein <MaxWinterstein@users.noreply.github.com>
2024-12-29 20:58:31 +0100

    CI: Add code checks to MR

--- toogoodtogo_ha_mqtt_bridge/watchdog.py
+++ toogoodtogo_ha_mqtt_bridge/watchdog.py
@@ -1,22 +1,23 @@
 from threading import Timer
+from typing import Any, Callable
 
 
 class Watchdog(Exception):
     """Thx https://stackoverflow.com/a/16148744"""
 
-    def __init__(self, timeout, user_handler=None):  # timeout in seconds
+    def __init__(self, timeout: float, user_handler: Callable[[], Any] | None = None) -> None:  # timeout in seconds
         self.timeout = timeout
-        self.handler = user_handler if user_handler is not None else self.default_handler
+        self.handler: Callable[[], Any] = user_handler if user_handler is not None else self.default_handler
         self.timer = Timer(self.timeout, self.handler)
         self.timer.start()
 
-    def reset(self):
+    def reset(self) -> None:
         self.timer.cancel()
         self.timer = Timer(self.timeout, self.handler)
         self.timer.start()
 
-    def stop(self):
+    def stop(self) -> None:
         self.timer.cancel()
 
-    def default_handler(self):
+    def default_handler(self) -> None:
         raise self
